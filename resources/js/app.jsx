5cdc10ab345f11f40ebe08dd4ee0e375eebc76a7
Adham Faraitodi <109128115+adhamfaraitodi@users.noreply.github.com>
2025-01-08 23:07:08 +0700

    add reactjs and tailwind using vite as asset bundling

---
+++ resources/js/app.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const App = () => (
+    <h1 className="text-4xl font-bold text-blue-500">Hello Laravel + React + Vite!</h1>
+);
+
+ReactDOM.createRoot(document.getElementById('app')).render(<App />);
