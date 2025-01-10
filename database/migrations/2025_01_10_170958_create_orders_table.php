77d5cbb43997c37462438544e3dfc0b189db133d
Adham Faraitodi <109128115+adhamfaraitodi@users.noreply.github.com>
2025-01-11 01:44:57 +0700

    adding migration and model file

---
+++ database/migrations/2025_01_10_170958_create_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id('order_id');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('landingpage_id');
+            $table->unsignedBigInteger('delivery_id');
+            $table->string('company_profile',255);
+            $table->text('description');
+            $table->enum('payment_status',['pending', 'success', 'failed', 'canceled', 'deny']);
+            $table->enum('order_status',['pending', 'paid', 'processing', 'shipped', 'delivered', 'completed', 'canceled', 'failed', 'refunded', 'on_hold']);
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('user_id')->on('users')->onDelete('set null');
+            $table->foreign('landingpage_id')->references('landingpage_id')->on('landing_pages')->onDelete('set null');
+            $table->foreign('delivery_id')->references('delivery_id')->on('deliveries')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
