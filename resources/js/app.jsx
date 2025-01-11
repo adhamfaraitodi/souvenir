af1e79ffec4232792e292e483044230e3bdcf5ec
rianwpzx <rianwp2015@gmail.com>
2025-01-11 14:14:57 +0700

    Fix: react setup error

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import "./bootstrap";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
+import Home from "./Home";
 
 const App = () => (
-    <h1 className="text-4xl font-bold text-blue-500">Hello Laravel + React + Vite!</h1>
+    <h1 className="text-4xl font-bold text-blue-500">
+        <Home />
+    </h1>
 );
 
-ReactDOM.createRoot(document.getElementById('app')).render(<App />);
+ReactDOM.createRoot(document.getElementById("app")).render(<App />);
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,19 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [
+        react(),
         laravel({
-            input: ['resources/js/app.jsx', 'resources/css/app.css'],
+            input: ["resources/js/app.jsx", "resources/css/app.css"],
             refresh: true,
         }),
-        react(),
     ],
+    server: {
+        host: "0.0.0.0",
+        hmr: {
+            host: "localhost",
+        },
+    },
 });
