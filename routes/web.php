c48881a19dd0e4a6cb32adff7916fa98b3d0e77e
Adham Faraitodi <109128115+adhamfaraitodi@users.noreply.github.com>
2025-01-14 01:48:07 +0700

    add userlandingpage &user order controller and add routing for it

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\UserLandingPage;
+use App\Http\Controllers\UserOrder;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () {return view('welcome');});
+Route::resource('order', UserOrder::class);
+Route::resource('landing-page',UserLandingPage::class);
